27bd8943772774272655be75acb777ec8f4c5cc9
isaiahsilvani <isaiahsilvani@outlook.com>
2025-03-12 16:57:10 -0400

    remove unused imports

--- app/src/main/java/com/example/walmartcodingtest/presentation/viewmodel/CountriesViewModelFactory.kt
+++ app/src/main/java/com/example/walmartcodingtest/presentation/viewmodel/CountriesViewModelFactory.kt
@@ -1,9 +1,7 @@
 package com.example.walmartcodingtest.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.walmartcodingtest.data.remote.RetrofitProvider
 import com.example.walmartcodingtest.data.repositories.CountryRepoImpl
 import com.example.walmartcodingtest.domain.usecases.GetCountries
